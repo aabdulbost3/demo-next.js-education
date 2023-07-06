2273e48d13124d819bf6dd5d7d60b73d36396493
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-07-07 03:56:11 +0500

    to learn Link tags

--- apps/app/nav/page.js
+++ apps/app/nav/page.js
@@ -1,4 +1,5 @@
 import style from "./Nav.module.css"
+import Link from "next/link"
 export default function Nav() {
     return (
         <>
@@ -11,10 +12,10 @@ export default function Nav() {
 
                         <div>
                             <ul className={style.navList}>
-                                <li className={style.navText}>Home</li>
-                                <li className={style.navText}>About</li>
-                                <li className={style.navText}>Contact</li>
-                                <li className={style.navText}>FAQ</li>
+                                <li className={style.navText}><Link href="/">Home</Link></li>
+                                <li className={style.navText}><Link href="dashboard">dashboard</Link></li>
+                                <li className={style.navText}><Link href="/">Contact</Link></li>
+                                <li className={style.navText}><Link href="/">FAQ</Link></li>
                             </ul>
                         </div>
                     </div>
