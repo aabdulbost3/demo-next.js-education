dd9b1870e9935aaac464cf9ce0b971266d076915
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-07-07 04:45:20 +0500

    img for src

--- apps/app/dashboard/page.js
+++ apps/app/dashboard/page.js
@@ -7,7 +7,7 @@ export default function Page() {
     Hello, Dashboard Page!
   </h1>
   <img src="https://c4.wallpaperflare.com/wallpaper/431/451/684/the-most-beautiful-picture-of-nature-wallpaper-preview.jpg"/>
-
+  
   </>
   )
 }
