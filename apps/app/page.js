7db42da1323d076fb25af4548b335135ca2ba6f9
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-07-08 03:04:29 +0500

    edite file code

--- apps/app/page.js
+++ apps/app/page.js
@@ -1,12 +1,11 @@
-import PostRouting from "./post/[Id]/page.js"
 import Link from "next/link.js"
 export default function Home() {
   return (
     <div>
       <h1>Hello, Home page!</h1>
       <Link href="/post/blog">blogga otish</Link>
-      {/* <PostRouting /> */}
     </div>
   )
 }
 // day 2
+// day 3
