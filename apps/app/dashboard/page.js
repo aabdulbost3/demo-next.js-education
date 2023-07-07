6fcdfe9e57607509ca40d86762b78a13fb6f1a3c
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-07-07 05:13:36 +0500

    img paste, img get internet and img layout

--- apps/app/dashboard/page.js
+++ apps/app/dashboard/page.js
@@ -1,13 +1,15 @@
 import "./style.css"
-// import Image from "next/image"
+import Image from "next/image"
+import imageTest from "./Public/img.jpg"
 export default function Page() {
   return (
   <>
   <h1>
     Hello, Dashboard Page!
   </h1>
-  <img src="https://c4.wallpaperflare.com/wallpaper/431/451/684/the-most-beautiful-picture-of-nature-wallpaper-preview.jpg"/>
-  
+  <img src="https://c4.wallpaperflare.com/wallpaper/431/451/684/the-most-beautiful-picture-of-nature-wallpaper-preview.jpg" layout="intrinsic" width={730} height={500} />
+  <Image src={imageTest} layout="responsive" width={730} height={500} />  {/*100% holatda responsive qiladi */}
+  <Image src={imageTest} layout="intrinsic" width={730} height={500} />    {/* malum belgilanga width da va height da boladi va oshmaydi*/}
   </>
   )
 }
