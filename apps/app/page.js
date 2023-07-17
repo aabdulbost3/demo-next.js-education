6e95268cfb4cd7ff75547e8eeca4719161f0fcef
Abdulbosit <96017917+aabdulbost3@users.noreply.github.com>
2023-07-18 00:50:05 +0500

    find eror 2

--- apps/app/page.js
+++ apps/app/page.js
@@ -10,3 +10,4 @@ export default function Home() {
 // day 2
 // day 3
 // day 4
+// day 5
--- apps/app/studentListRoutingFile/[student]/page.js
+++ apps/app/studentListRoutingFile/[student]/page.js
@@ -1,14 +1,12 @@
 'use client'
 
-
-
 export default function Student({params}) {
     console.log(params);
 
     return(
         <div>
             <h1>Student Detalis</h1>
-            <h3>Name : {params.student}</h3>
+            <h3>Name : {params.Student}</h3>
             
         </div>
     )
